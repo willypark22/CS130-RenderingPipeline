539193adad741b81b6e9f02feb6a0a4b0775c6e0
Dong Park <dpark032@bolt.cs.ucr.edu>
2019-02-25 12:49:12 -0800

    got up to 21 points so far

--- proj-gl-files/driver_state.cpp
+++ proj-gl-files/driver_state.cpp
@@ -39,30 +39,33 @@ void initialize_render(driver_state& state, int width, int height)
 void render(driver_state& state, render_type type)
 {
     //std::cout<<"TODO: implement rendering."<<std::endl;
-    switch(type) {
+
+    for(int i = 0; i <= (state.num_vertices * 3); i += (3 * state.floats_per_vertex)) {
+    	switch(type) {
 		case render_type::triangle:
 		{
 			const data_geometry *vertexArray[3];
 			data_geometry temp[3];
 			data_vertex a, b, c;
-	
-			a.data = &state.vertex_data[0];
-			b.data = &state.vertex_data[1 * state.floats_per_vertex];
-			c.data = &state.vertex_data[2 * state.floats_per_vertex];
-	
+		
+			a.data = &state.vertex_data[i];
+			b.data = &state.vertex_data[i + (1 * state.floats_per_vertex)];
+			c.data = &state.vertex_data[i + (2 * state.floats_per_vertex)];
+		
 			temp[0].data = a.data;
 			temp[1].data = b.data;
 			temp[2].data = c.data;
-	
+		
 			state.vertex_shader(a, temp[0], state.uniform_data);
 			state.vertex_shader(b, temp[1], state.uniform_data);
 			state.vertex_shader(c, temp[2], state.uniform_data);
-	
+		
 			for(int i = 0; i < 3; i++) {
 				vertexArray[i] = &temp[i];
 			}	
-			
+				
 			rasterize_triangle(state, vertexArray);
+	
 			break;
 		}
 		
@@ -80,6 +83,7 @@ void render(driver_state& state, render_type type)
 		{
 			//
 		}
+	}
     }
 }
 
@@ -108,42 +112,32 @@ void rasterize_triangle(driver_state& state, const data_geometry* in[3])
     
     float aX = 0, aY = 0, bX = 0, bY = 0, cX = 0, cY = 0;
     float alpha = 0, beta = 0, gamma = 0;
-    float alpha0 = 0, alpha1 = 0, alpha2 = 0, beta0 = 0, beta1 = 0, beta2 = 0, gamma0 = 0, gamma1 = 0, gamma2 = 0;
     
     auto w = state.image_width;
     auto h = state.image_height;
     
-    aX = (w/2) * (*in)[0].gl_Position[0] + ((w/2) - (1/2));
-    bX = (w/2) * (*in)[1].gl_Position[0] + ((w/2) - (1/2));
-    cX = (w/2) * (*in)[2].gl_Position[0] + ((w/2) - (1/2));
-    aY = (h/2) * (*in)[0].gl_Position[1] + ((h/2) - (1/2));
-    bY = (h/2) * (*in)[1].gl_Position[1] + ((h/2) - (1/2));
-    cY = (h/2) * (*in)[2].gl_Position[1] + ((h/2) - (1/2));
+    aX = (w/2) * in[0]->gl_Position[0] + ((w/2) - (1/2));
+    aY = (h/2) * in[0]->gl_Position[1] + ((h/2) - (1/2));
+    bX = (w/2) * in[1]->gl_Position[0] + ((w/2) - (1/2));
+    bY = (h/2) * in[1]->gl_Position[1] + ((h/2) - (1/2));
+    cX = (w/2) * in[2]->gl_Position[0] + ((w/2) - (1/2));
+    cY = (h/2) * in[2]->gl_Position[1] + ((h/2) - (1/2));
     
-    float area = 0.5f * (((bX * cY) - (cX * bY)) - ((aX * cY) - (cX * aY)) - ((aX * bY) - (bX * aY)));
-    
-    alpha0 = (bX * cY - cX * bY);
-    alpha1 = (bY - cY);
-    alpha2 = (cX - bX);
-    
-    beta0 = (cX * aY - aX * cY);
-    beta1 = (cY - aY);
-    beta2 = (aX - cX);
-	
-    gamma0 = (aX * bY - bX * aY);
-    gamma1 = (aY - bY);
-    gamma2 = (bX - aX);
-    
-    for(auto j = 0; j < state.image_height; ++j) {
-		for(auto i = 0; i < state.image_width; ++i) {
-			alpha = (0.5f * (alpha0 + alpha1 * i + alpha2 * j))/area;
-			beta = (0.5f * (beta0 + beta1 * i + beta2 * j))/area;
-			gamma = (0.5f * (gamma0 + gamma1 * i + gamma2 * j))/area;
-			if(alpha >= 0 && beta >= 0 && gamma >= 0) {
-				state.image_color[i + j * w] = make_pixel(255.0, 255.0, 255.0);
-			}
+    for(auto i = 0; i < w; i++) {
+	float area = (cX - aX) * (bY - aY) - (cY - aY) * (bX - aX);
+	for(auto j = 0; j < h; j++) {
+		alpha = (i - bX) * (cY - bY) - (j - bY) * (cX - bX); 
+		beta = (i - cX) * (aY - cY) - (j - cY) * (aX - cX);
+		gamma = (i - aX) * (bY - aY) - (j - aY) * (bX - aX);
+		alpha /= area;
+		beta /= area;
+		gamma /= area;
+
+		if(alpha >= 0 && beta >= 0 && gamma >= 0) {
+			state.image_color[i + j * w] = make_pixel(255.0, 255.0, 255.0);
 		}
-     }
+	}
+    }
     
 }
 
